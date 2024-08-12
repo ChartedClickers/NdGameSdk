7c7d3e2f78aabe0f3b34b785e53eff43dade1e03
VIPO777 <37861613+VIPO777@users.noreply.github.com>
2024-08-12 04:42:04 +0300

    Fix `SdkAction` event placeholders

--- shared/NdGameSdk/components/event/SdkAction.hpp
+++ shared/NdGameSdk/components/event/SdkAction.hpp
@@ -35,8 +35,25 @@ namespace NdGameSdk {
             std::memcpy(&funcAddress, &memberFunc, sizeof(funcAddress));
 
             if constexpr (sizeof...(Args) > 0) {
-                constexpr _Ph<sizeof...(Args)> _argscount{};
-                _function = boost::bind(memberFunc, instance, _argscount);
+                if constexpr (sizeof...(Args) == 1) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1);
+                } else if constexpr (sizeof...(Args) == 2) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1, boost::placeholders::_2);
+                } else if constexpr (sizeof...(Args) == 3) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3);
+                } else if constexpr (sizeof...(Args) == 4) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4);
+                } else if constexpr (sizeof...(Args) == 5) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4, boost::placeholders::_5);
+                } else if constexpr (sizeof...(Args) == 6) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4, boost::placeholders::_5, boost::placeholders::_6);
+                } else if constexpr (sizeof...(Args) == 7) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4, boost::placeholders::_5, boost::placeholders::_6, boost::placeholders::_7);
+                } else if constexpr (sizeof...(Args) == 8) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4, boost::placeholders::_5, boost::placeholders::_6, boost::placeholders::_7, boost::placeholders::_8);
+                } else if constexpr (sizeof...(Args) == 9) {
+                    _function = boost::bind(memberFunc, instance, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4, boost::placeholders::_5, boost::placeholders::_6, boost::placeholders::_7, boost::placeholders::_8, boost::placeholders::_9);
+                }
             }
             else {
                 _function = boost::bind(memberFunc, instance);
